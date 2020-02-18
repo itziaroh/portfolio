dee40f3a9fc00c547d7dd33c407aec2dbe940f3a
Itziar Ortega <itziarortega94@gmail.com>
2020-02-18 13:43:26 +0100

    Header layout

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <div className="about">
-            <h1>¡Hola!</h1>
+            <h1 className="about_hello">¡Hola!</h1>
+            <p>Me llamo Itziar y soy programadora frontend en Madrid.</p>
+            <p>Me encanta hacer aplicaciones web, principalmente usando React.</p>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="menu">
-            {/* <img src="img/" alt="logo_itziar" className="menu_logo" /> */}
-            <ul className="menu_list">
-                <li className="menu_list-item">Sobre mi</li>
-                <li className="menu_list-item">Proyectos</li>
-                <li className="menu_list-item">Skills</li>
-                <li className="menu_list-item">Contacto</li>
-            </ul>
-        </nav>
+        <header>
+            <div className="menu_wrapper">
+                <nav className="menu">
+                    <ul className="menu_list">
+                        <li className="menu_list-item">Sobre mi</li>
+                        <li className="menu_list-item">Proyectos</li>
+                        <li className="menu_list-item">Skills</li>
+                        <li className="menu_list-item">Contacto</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
