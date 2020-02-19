6b7b469d722d6d1139c982b6854ec67d38e1ea4c
Itziar Ortega <itziarortega94@gmail.com>
2020-02-19 13:18:28 +0100

    add docs

---
+++ docs/precache-manifest.4abf090e3c5d186d00add4634666cf6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752f70dd2bded968f2ac240c94dc01e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c5df0cf8a6a50f16591",
+    "url": "./static/css/main.1a23ffdb.chunk.css"
+  },
+  {
+    "revision": "ad8e8dccf92391a62d32",
+    "url": "./static/js/2.50fdd86e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.50fdd86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5df0cf8a6a50f16591",
+    "url": "./static/js/main.06e9911f.chunk.js"
+  },
+  {
+    "revision": "3cffdea7db2cc37140c8",
+    "url": "./static/js/runtime-main.692de215.js"
+  }
+]);
